d926229284bc4b94337a2be1aec598a3f8682c4e
Yazan Tarifi <yazantarifi98@gmail.com>
2021-07-17 14:54:15 -0700

    Generate Project Files

---
+++ app/src/main/java/com/yazantarifi/android/issuer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yazantarifi.android.issuer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
